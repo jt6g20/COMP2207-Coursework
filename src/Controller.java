713c2c0b784429656dd312211342416af077ed85
Josh Tucker <jt6g20@soton.ac.uk>
2022-04-30 17:24:12 +0100

    Started controller

--- src/Controller.java
+++ src/Controller.java
@@ -1,2 +1,22 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 public class Controller {
+    public static void main(String[] args) throws IOException {
+        //Port to listen to
+        String cport = args[0];
+        //Replication factor
+        String rFactor = args[1];
+        //Timeout in milliseconds
+        String timeout = args[2];
+        //How long to wait (in seconds) to start the next rebalance operation
+        String rebalPeriod = args[3];
+
+        System.out.println(cport + " " + rFactor + " " + timeout + " " + rebalPeriod);
+
+        List<String> index = new ArrayList<>();
+
+
+    }
 }
