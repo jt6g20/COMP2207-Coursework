9533ba9f0fe5f66b3538cfaea7de3314e4b39916
Josh Tucker <jt6g20@soton.ac.uk>
2022-04-30 18:56:31 +0100

    Added List operation to controller

--- src/Controller.java
+++ src/Controller.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Controller {
     public static void main(String[] args) throws IOException {
         //Port to listen to
-        String cport = args[0];
+        int cport = Integer.parseInt(args[0]);
         //Replication factor
         String rFactor = args[1];
         //Timeout in milliseconds
@@ -15,8 +15,61 @@ public class Controller {
 
         System.out.println(cport + " " + rFactor + " " + timeout + " " + rebalPeriod);
 
-        List<String> index = new ArrayList<>();
+        Map<String, String> index = new HashMap<>();
+
+        index.put("oogabooa.txt", "store complete");
+        index.put("weewoo.txt", "store complete");
+        index.put("fangfong.txt", "store complete");
+
+        try {
+            ServerSocket socket = new ServerSocket(cport);
+            for(;;) {
+                try {
+                    System.out.println("waiting for connection");
+                    Socket client = socket.accept();
+                    try {
+                        System.out.println("connected");
+                        InputStream in = client.getInputStream();
+                        byte[] buf = new byte[1000];
+                        int buflen = in.read(buf);
+                        String firstBuffer = new String(buf,0,buflen);
+                        System.out.println("INPUT - " + firstBuffer);
+//                        int firstSpace = firstBuffer.indexOf(" ");
+//                        String command = firstBuffer.substring(0,firstSpace);
+
+                        if (firstBuffer.startsWith("LIST")) {
+                            String file_list = "LIST " + listToString(index.keySet());
+                            System.out.println(file_list);
+                            PrintStream ps = new PrintStream(client.getOutputStream());
+                            ps.println(file_list);
+                            ps.close();
+                        }
+
+//                        if (command.equals("STORE")) {
+//                            int secondSpace = firstBuffer.indexOf(" ",firstSpace + 1);
+//                            String fileName = firstBuffer.substring(firstSpace+1, secondSpace);
+//                            int thirdSpace = firstBuffer.indexOf(" ", secondSpace + 1);
+//                            String fileSize = firstBuffer.substring(secondSpace+1, thirdSpace);
+//                            System.out.println("STORE " + fileName + " " + fileSize);
+//                            index.put(fileName, "store in progress");
+//                            in.close(); client.close();
+//                        }
 
 
+
+                        in.close(); client.close();
+
+                    } catch (Exception e) {}
+                } catch (Exception e) { System.out.println("error "+e); }
+            }
+        } catch (Exception e) { System.out.println("error "+e); }
+        System.out.println();
+    }
+
+    static String listToString(Set<String> l) {
+        StringBuilder sb = new StringBuilder("");
+        for (String s : l) sb.append(" ").append(s);
+        sb.deleteCharAt(0);
+        return sb.toString();
     }
 }
